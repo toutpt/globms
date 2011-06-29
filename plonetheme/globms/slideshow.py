a875187fbdd315862096f4d1c217f528693ca05b
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-06-30 00:18:18 +0200

    split slideshow for home page and for the rest of the site

--- plonetheme/globms/slideshow.py
+++ plonetheme/globms/slideshow.py
@@ -2,9 +2,32 @@ from zope import component
 
 from plone.app.layout.viewlets import common
 
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class SlideshowViewletRoot(common.ViewletBase):
+    def slides(self):
+        slides = []
+        slideshow = getattr(self.context,'slideshow',None)
+        if slideshow is None:
+            return []
+        slide_ids = slideshow.objectIds()
+        for slide_id in slide_ids:
+            slide = getattr(slideshow,slide_id)
+            slide_url = slide.absolute_url()
+            slides.append({'src':slide_url+'/image_thumb','url':slide_url,
+                           'title':slide.Title(),
+                           'description':slide.Description()})
+        return slides
+
+    def block(self):
+        block = getattr(self.context,'home-slide-block')
+        return {'title':block.Title(),
+                'description':block.Description(),
+                'img':self.context.absolute_url()+'/home-slide-block.jpg'}
+
 class SlideshowViewlet(common.ViewletBase):
     """Viewlet for slideshow on many pages"""
-    
+
     def index(self):
         portal_state = component.getMultiAdapter((self.context, self.request),
                                          name=u'plone_portal_state')
@@ -12,19 +35,10 @@ class SlideshowViewlet(common.ViewletBase):
                                          name=u'plone_context_state')
         portal = portal_state.portal()
         context = self.context.aq_base
-        if context_state.is_portal_root():
-#            self.context.plone_log('we are on root')
-            if 'blocks' not in portal.objectIds():
-#                self.context.plone_log('no blocks folder')
-                return u""
-            if not context_state.is_view_template():
-                return u""
-            slide = portal.blocks.home_slideshow
-        else:
-#            self.context.plone_log('we are not on root')
-            if context_state.is_default_page:
-                context = context_state.parent()
-            slide = getattr(context,'bandeau.png', None)
-            if slide is None:
-                return u""
+        if context_state.is_default_page:
+            context = context_state.parent()
+        slide = getattr(context,'bandeau.png', None)
+        if slide is None:
+            return u""
         return slide.restrictedTraverse('slideshow.html')()
+
