b4981f8e6fab73ddaf5a69e8e44fa21c149b0237
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-08-05 13:50:05 +0200

    force intranet and extranet links to point into /en

--- plonetheme/globms/personal_bar.py
+++ plonetheme/globms/personal_bar.py
@@ -4,3 +4,11 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class PersonalBarViewlet(common.PersonalBarViewlet):
     index = ViewPageTemplateFile('personal_bar.pt')
+    
+    def extranet_url(self):
+        portal_url = self.portal_state.portal_url()
+        return portal_url + '/en/intranet'
+
+    def intranet_url(self):
+        portal_url = self.portal_state.portal_url()
+        return portal_url + '/en/intranet'
